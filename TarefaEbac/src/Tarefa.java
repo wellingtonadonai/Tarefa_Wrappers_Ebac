2a6cffcf21ad03e495f644122f250969dfd5d229
wellington <wellington.h.s.oliveira@hotmail.com>
2024-07-12 11:08:34 -0300

    Tarefa Wrappers

---
+++ TarefaEbac/src/Tarefa.java
@@ -0,0 +1,12 @@
+public class Tarefa {
+
+    public static void main(String[] args) {
+
+        int num = Integer.valueOf(200);
+        boolean status = Boolean.TRUE;
+
+        System.out.println(num);
+        System.out.println(status);
+
+    }
+}
